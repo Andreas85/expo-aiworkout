8d4e2f53d0dbe4a7974a65173320d65f866c7ee7
Saurabh Shukla <saurabhshukla3107@gmail.com>
2024-08-27 19:34:38 +0530

    UI improvements

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -2,7 +2,7 @@ import Container from '@/components/atoms/Container';
 import { Text, View } from '@/components/Themed';
 import { useFetchData } from '@/hooks/useFetchData';
 import { tailwind } from '@/utils/tailwind';
-import { ScrollView } from 'react-native';
+import { Image, ScrollView } from 'react-native';
 
 export default function HomeIndexPage() {
   const { data, error, isPending } = useFetchData({
@@ -18,9 +18,9 @@ export default function HomeIndexPage() {
   if (error) return <Text>An error has occurred: + {error.message}</Text>;
 
   return (
-    <View style={tailwind('h-full')}>
+    <View style={tailwind('h-full bg-transparent')}>
       <ScrollView>
-        <Container style={tailwind('flex-1 gap-y-4')} className="flex flex-1 flex-col gap-2">
+        <Container style={tailwind(' gap-y-4')} className="flex flex-1 flex-col gap-2">
           <Container
             style={tailwind('flex h-14 gap-y-4')}
             className="flex items-center justify-between">
@@ -36,9 +36,17 @@ export default function HomeIndexPage() {
           </Container>
           <Container style={tailwind('border border-white')} className="border border-white" />
         </Container>
-        <Container className="flex-1 flex-col gap-4">
-          <Text>{JSON.stringify(data)}</Text>
-          <Text>{JSON.stringify(data)}</Text>
+        <Container
+          style={tailwind('gap-y-2 ')}
+          className="my-4 grid grid-cols-2 gap-4  md:grid-cols-3 lg:grid-cols-4">
+          {data.map((item: any, index: number) => {
+            return (
+              <Container key={index} style={tailwind('')} className="self-center rounded-lg ">
+                <Image source={{ uri: item?.image }} style={tailwind('h-64 w-full rounded-lg')} />
+                <Text style={tailwind('self-center text-center')}>{item?.title?.slice(0, 25)}</Text>
+              </Container>
+            );
+          })}
         </Container>
       </ScrollView>
     </View>
