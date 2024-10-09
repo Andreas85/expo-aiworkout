e0629b3ce1290a8d1ed905bd7512b9477b0f557c
Saurabh Shukla <saurabhshukla3107@gmail.com>
2024-10-09 10:53:42 +0530

    Tabs Black background color issue fixed

--- app/(tabs)/(workout)/_layout.tsx
+++ app/(tabs)/(workout)/_layout.tsx
@@ -9,6 +9,7 @@ import useBreakPoints from '@/hooks/useBreakPoints';
 import { useAuthStore } from '@/store/authStore';
 import WorkoutIndexRoute from './workouts';
 import PublicScreenRoute from './workouts/public';
+import GradientBackground from '@/components/atoms/GradientBackground';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -17,65 +18,74 @@ export default function Layout() {
   const { isLargeScreen } = useBreakPoints();
   const { isAuthenticated } = useAuthStore();
   return (
-    <SafeAreaView style={[tailwind('flex-1'), { marginTop: insets.top }]}>
-      <Container style={[tailwind('flex-1'), { marginTop: insets.bottom }]}>
-        <Tab.Navigator
-          sceneContainerStyle={Platform.select({
-            web: tailwind('bg-transparent '),
-            native: tailwind('flex-1 bg-transparent'),
-          })}
-          // initialRouteName="workout"
-          screenOptions={({ route }) => {
-            // console.log(route, 'route');
+    <SafeAreaView style={[tailwind('flex-1')]}>
+      <GradientBackground
+        styleWeb="!mt-0"
+        styleNative={[
+          Platform.select({
+            web: tailwind(`${!isLargeScreen && !isAuthenticated ? 'mt-24' : ''} flex-1`),
+            native: tailwind('flex-1'),
+          }),
+        ]}>
+        <Container style={[tailwind('flex-1'), { marginTop: insets.bottom }]}>
+          <Tab.Navigator
+            sceneContainerStyle={Platform.select({
+              web: tailwind('bg-transparent '),
+              native: tailwind('flex-1 bg-transparent'),
+            })}
+            // initialRouteName="workout"
+            screenOptions={({ route }) => {
+              // console.log(route, 'route');
 
-            return {
-              tabBarStyle: Platform.select({
-                web: tailwind(
-                  `rounded-t-4 mx-auto 
+              return {
+                tabBarStyle: Platform.select({
+                  web: tailwind(
+                    `rounded-t-4 mx-auto 
                   ${isLargeScreen ? 'mt-4' : 'mt-32'} w-80
                     capitalize 
                   ${!isAuthenticated && 'hidden'}
                   `,
-                ),
-                native: {
-                  // backgroundColor: '#493B42', // Set the background color based on the selected tab
+                  ),
+                  native: {
+                    // backgroundColor: '#493B42', // Set the background color based on the selected tab
+                    borderTopLeftRadius: 24,
+                    borderTopRightRadius: 24,
+                    marginHorizontal: 16,
+                    display: !isAuthenticated ? 'none' : 'flex',
+                  },
+                }),
+                tabBarIndicatorStyle: {
+                  backgroundColor: '#9C79C9', // Set the background color for the selected tab's indicator
+                },
+                tabBarIndicatorContainerStyle: {
                   borderTopLeftRadius: 24,
                   borderTopRightRadius: 24,
-                  marginHorizontal: 16,
-                  display: !isAuthenticated ? 'none' : 'flex',
                 },
-              }),
-              tabBarIndicatorStyle: {
-                backgroundColor: '#9C79C9', // Set the background color for the selected tab's indicator
-              },
-              tabBarIndicatorContainerStyle: {
-                borderTopLeftRadius: 24,
-                borderTopRightRadius: 24,
-              },
-              tabBarLabel: (props: { focused: boolean; color: string; children: string }) => {
-                const { children } = props;
-                return <Text>{children}</Text>;
-              },
-            };
-          }}>
-          {/* {isAuthenticated && ( */}
-          <Tab.Screen
-            name="workouts/index"
-            component={isAuthenticated ? WorkoutIndexRoute : PublicScreenRoute}
-            options={({ route }) => ({
-              title: 'My workouts',
-            })}
-          />
-          {/* )} */}
-          <Tab.Screen
-            name="workouts/public"
-            component={PublicScreenRoute}
-            options={({ route }) => ({
-              title: 'Public workouts',
-            })}
-          />
-        </Tab.Navigator>
-      </Container>
+                tabBarLabel: (props: { focused: boolean; color: string; children: string }) => {
+                  const { children } = props;
+                  return <Text>{children}</Text>;
+                },
+              };
+            }}>
+            {/* {isAuthenticated && ( */}
+            <Tab.Screen
+              name="workouts/index"
+              component={isAuthenticated ? WorkoutIndexRoute : PublicScreenRoute}
+              options={({ route }) => ({
+                title: 'My workouts',
+              })}
+            />
+            {/* )} */}
+            <Tab.Screen
+              name="workouts/public"
+              component={PublicScreenRoute}
+              options={({ route }) => ({
+                title: 'Public workouts',
+              })}
+            />
+          </Tab.Navigator>
+        </Container>
+      </GradientBackground>
     </SafeAreaView>
   );
 }
--- app/(tabs)/(workout)/workouts/index.tsx
+++ app/(tabs)/(workout)/workouts/index.tsx
@@ -1,11 +1,6 @@
-import GradientBackground from '@/components/atoms/GradientBackground';
 import MyWorkout from '@/components/screens/MyWorkout';
 import React from 'react';
 
 export default function WorkoutIndexRoute() {
-  return (
-    <GradientBackground styleWeb="!mt-0">
-      <MyWorkout />
-    </GradientBackground>
-  );
+  return <MyWorkout />;
 }
