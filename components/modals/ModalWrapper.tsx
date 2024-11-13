279b3e34c10aeddc9fa574fbb1cb7fb79316176d
Saurabh Shukla <saurabhshukla3107@gmail.com>
2024-11-13 12:59:43 +0530

    ModalWrapper changes. to fix scroll issue in native

--- components/modals/ModalWrapper.tsx
+++ components/modals/ModalWrapper.tsx
@@ -4,8 +4,10 @@ import Container from '../atoms/Container';
 import TextContainer from '../atoms/TextContainer';
 import { tailwind } from '@/utils/tailwind';
 import { IAddAndEditWorkoutModalProps } from '@/utils/interfaces';
-import { Platform, ScrollView } from 'react-native';
+import { Platform, ScrollView, View, Dimensions } from 'react-native';
 import useWebBreakPoints from '@/hooks/useWebBreakPoints';
+
+const { height: screenHeight } = Dimensions.get('window');
 
 function ModalWrapper(props: IAddAndEditWorkoutModalProps) {
   const { isModalVisible, closeModal, headerTitle, children, footerChildren } = props;
@@ -21,31 +23,39 @@ function ModalWrapper(props: IAddAndEditWorkoutModalProps) {
         useNativeDriver={true}
         animationIn="fadeIn"
         animationOut="fadeOut">
-        <ScrollView
-          contentContainerStyle={{
+        <View
+          style={{
             flex: 1,
             justifyContent: 'center',
+            paddingHorizontal: 16,
           }}>
-          <Container
-            style={Platform.select({
-              web: tailwind(
-                `flex gap-4 rounded-xl  bg-black p-6
-                ${isExtraLargeScreenOnly && 'm-auto w-2/5 '} 
-                ${isLargeAndMediumScreen && 'm-auto w-3/5 '} 
-                ${isExtraSmallScreenOnly && 'm-auto w-11/12 '}
-              `,
-              ),
-              native: tailwind('flex gap-4 rounded-xl bg-black p-6'),
-            })}>
-            {headerTitle && (
-              <Container style={tailwind('flex items-center justify-center')}>
-                <TextContainer style={tailwind('text-lg font-bold')} data={headerTitle} />
-              </Container>
-            )}
-            <Container style={tailwind('')}>{children}</Container>
-            <Container style={tailwind('')}>{footerChildren}</Container>
-          </Container>
-        </ScrollView>
+          <ScrollView
+            contentContainerStyle={{
+              flexGrow: 1,
+              justifyContent: 'center',
+            }}
+            style={{ maxHeight: screenHeight * 0.8 }}>
+            <Container
+              style={Platform.select({
+                web: tailwind(
+                  `flex gap-4 rounded-xl bg-black p-6
+                   ${isExtraLargeScreenOnly && 'm-auto w-2/5 '} 
+                   ${isLargeAndMediumScreen && 'm-auto w-3/5 '} 
+                   ${isExtraSmallScreenOnly && 'm-auto w-11/12 '}
+                 `,
+                ),
+                native: tailwind('flex gap-4 rounded-xl bg-black p-6'),
+              })}>
+              {headerTitle && (
+                <Container style={tailwind('mb-4 flex items-center justify-center')}>
+                  <TextContainer style={tailwind('text-lg font-bold')} data={headerTitle} />
+                </Container>
+              )}
+              <Container style={tailwind('flex-grow')}>{children}</Container>
+              <Container style={tailwind('mt-4')}>{footerChildren}</Container>
+            </Container>
+          </ScrollView>
+        </View>
       </Modal>
     </Container>
   );
