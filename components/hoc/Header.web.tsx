ede9e6d91b137e84b3dea0fa0fd2e4c823e84d9f
Andreas Larsson <andx85@gmail.com>
2025-07-03 12:04:42 +0200

    Make logo click go to landing page/workouts depending on auth

--- components/hoc/Header.web.tsx
+++ components/hoc/Header.web.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Href, Link, router, usePathname } from 'expo-router';
 import { tailwind } from '@/utils/tailwind';
 import { ActionButton } from '../atoms/ActionButton';
-import { Image } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import { IMAGES } from '@/utils/images';
 import { useAuthStore } from '@/store/authStore';
 import { ITabItem } from '@/utils/interfaces';
@@ -15,6 +15,14 @@ const Header = () => {
   const pathname = usePathname() as Href<string | object>;
   const [activeTab, setActiveTab] = useState<Href<string | object>>('/'); // Default active tab
   const [navItems] = useState<ITabItem[]>(headerOptions);
+
+  const handleLogoClick = () => {
+    if (isAuthenticated) {
+      router.push('/(tabs)/(workout)/workout');
+    } else {
+      router.push('/landing');
+    }
+  };
 
   const renderActionButtonAndLinks = () => {
     if (!isAuthenticated) {
@@ -76,12 +84,12 @@ const Header = () => {
           'fixed left-0 right-0 top-0 z-50 flex h-24 flex-row items-center justify-between gap-4 bg-NAVBAR_BACKGROUND px-4 lg:px-32'
         }>
         <span>
-          <Link href="/(tabs)/(workout)/">
+          <TouchableOpacity onPress={handleLogoClick}>
             <Image
               source={IMAGES.logo}
               style={tailwind('aspect-video h-16 w-20 w-auto md:h-auto')}
             />
-          </Link>
+          </TouchableOpacity>
         </span>
         {renderActionButtonAndLinks()}
       </div>
